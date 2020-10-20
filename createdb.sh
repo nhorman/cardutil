aefb9f8d6ece5393e9681a2791455c8fdf22a958
Neil Horman <nhorman@gmail.com>
2020-10-20 15:49:19 -0400

    misc stuff

--- createdb.sh
+++ createdb.sh
@@ -5,6 +5,6 @@ rm -f $1
 
 echo "CREATE TABLE locations (location TEXT NOT NULL PRIMARY KEY, longitude REAL NOT NULL, latitude REAL NOT NULL , longdir TEXT, latdir TEXT, width INTEGER NOT NULL, height INTEGER NOT NULL, UNIQUE(location));" | sqlite3 $1
 
-echo "CREATE TABLE card (rowId INTEGER PRIMARY KEY AUTOINCREMENT, level INTEGER, cardlogo BLOB, year INTEGER, location TEXT, category TEXT, cardtext TEXT, credittext TEXT, crediturl TEXT, FOREIGN KEY(location) REFERENCES locations(location));" | sqlite3 $1
+echo "CREATE TABLE card (rowId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, level INTEGER, cardlogo BLOB, year INTEGER, location TEXT, category TEXT, cardtext TEXT, credittext TEXT, crediturl TEXT, FOREIGN KEY(location) REFERENCES locations(location));" | sqlite3 $1
 
 
