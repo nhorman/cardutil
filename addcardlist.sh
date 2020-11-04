fda20fbc0c8d3f494c0bbc5a50901bd817c8020c
Neil Horman <nhorman@gmail.com>
2020-11-04 09:13:49 -0500

    Fixing stories test

--- addcardlist.sh
+++ addcardlist.sh
@@ -7,10 +7,12 @@ CATEGORY=`echo $LISTFILE | cut -d'.' -f1`
 
 for i in `cat $LIST`
 do
-	if [ "$LISTFILE" == "stories" ]
+	if [ "$CATEGORY" != "stories" ]
 	then
+		echo "Non-Story card"
 		./addcard.sh $DB $i $CATEGORY 1
 	else
+		echo "STORY CARD"
 		./addcard.sh $DB $i $CATEGORY 0
 	fi
 done
