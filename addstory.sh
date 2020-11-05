b5963df5e7fa24c46a6e6fef5a0631a8c9d8ecce
Neil Horman <nhorman@gmail.com>
2020-11-05 14:32:09 -0500

    Adding to story

--- addstory.sh
+++ addstory.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DB=$1
 JSONFILE=$2
-TMPDIR=$(mktemp -d addcard.XXXXXX)
+TMPDIR=$(mktemp -d addstory.XXXXXX)
 
 function cleanup()
 {
@@ -25,6 +25,15 @@ do
 	let PAGENUMBER=$i+1
 	echo "Building page $i for story $STORYNAME"
 	PAGEFILE=$(jq --argjson idx "$i" -r '.pages[$idx].pagefile' $JSONFILE)
+
+	# copy the html for the pages and convert any ANSWER tokens to input
+	BASEPAGEFILE=$(basename $PAGEFILE)
+	cp $PAGEFILE $TMPDIR/$BASEPAGEFILE
+	for p in `grep -o "ANSWER[0-9]\+" $TMPDIR/$BASEPAGEFILE | sort | uniq`
+	do 
+		sed -i -e"s/\(\\$\)\($p\)/<input type=text readOnly=true value=\2 class=\"ANSWER\"><\/input>/g" $TMPDIR/$BASEPAGEFILE
+	done
+	PAGEFILE=$TMPDIR/$BASEPAGEFILE
 	ANSWERFILE=$(jq --argjson idx "$i" -r '.pages[$idx].answerfile' $JSONFILE)
 	ANSWERORDERMATTERS=$(jq --argjson idx "$i" -r '.pages[$idx].answerordermatters' $JSONFILE)
 	CORRECTCARDS=""
@@ -48,7 +57,6 @@ do
 			CORRECTCARDS="$CORRECTCARDS:$NEWCARD"
 		fi
 	done
-
 	if [ "$ANSWERFILE" == "none" ]
 	then
 		echo "insert into storypage (pagenumber, storyname, storytext, correctcardname, answerordermatters, answertext) values ($PAGENUMBER, $STORYNAME, readfile(\"$PAGEFILE\"), \"$CORRECTCARDS\", $ANSWERORDERMATTERS, \"none\");" | sqlite3 $DB 
